fb97c194d6cd8b7b3607967bb7aaf630be66f0ef
Kwandes <bz.jasiek@gmail.com>
2022-08-23 13:51:31 +0200

    Add Go conversion of JSON file

--- exerciseUno/file-formats-bonanza/go/main.go
+++ exerciseUno/file-formats-bonanza/go/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+type planetsData []map[string]interface{}
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Go World")
+
+	var data planetsData
+	// Json Data
+	jsonFile, err := ioutil.ReadFile("../files/planets.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(jsonFile, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(data)
 }
