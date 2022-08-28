5d4381045041b6608a01ddeb6dc3a54ebce0988a
Kwandes <bz.jasiek@gmail.com>
2022-08-28 14:49:44 +0200

    Add Go conversion of YAML file

--- exerciseUno/file-formats-bonanza/go/go.mod
+++ exerciseUno/file-formats-bonanza/go/go.mod
@@ -1,3 +1,7 @@
 module bonanza
 
 go 1.19
+
+require github.com/sbabiv/xml2map v1.2.1
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
--- exerciseUno/file-formats-bonanza/go/main.go
+++ exerciseUno/file-formats-bonanza/go/main.go
@@ -5,12 +5,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+
+	"gopkg.in/yaml.v3"
+	// "strings"
+	// "github.com/sbabiv/xml2map"
 )
 
 type planetsData []map[string]interface{}
 
 func main() {
-	fmt.Println("Go World")
+	fmt.Println("===== JSON =====")
 
 	var data planetsData
 	// Json Data
@@ -23,4 +27,53 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(data)
+
+	// YAML Data
+	fmt.Println("===== YAML =====")
+	dataYaml, err := ioutil.ReadFile("../files/planets.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var body interface{}
+	if err := yaml.Unmarshal([]byte(dataYaml), &body); err != nil {
+		log.Fatal(err)
+	}
+
+	body = convertYamlToJson(body)
+
+	if b, err := json.Marshal(body); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("Output: %s\n", b)
+	}
+
+	// XML - doesn't work so it is commented out
+	// decoder := xml2map.NewDecoder(strings.NewReader("../files/planets.xml"))
+	// result, err := decoder.Decode()
+
+	// if err != nil {
+	// 	fmt.Printf("%v\n", err)
+	// } else {
+	// 	fmt.Printf("%v\n", result)
+	// }
+
+	// v := result["root"].(map[string]interface{})["row"].([]map[string]interface{})[0]["satelites"].(map[string]interface{})["n"].([]string)[1]
+
+	// fmt.Printf("n[1]: %v\n", v)
+}
+
+func convertYamlToJson(i interface{}) interface{} {
+	switch x := i.(type) {
+	case map[interface{}]interface{}:
+		m2 := map[string]interface{}{}
+		for k, v := range x {
+			m2[k.(string)] = convertYamlToJson(v)
+		}
+		return m2
+	case []interface{}:
+		for i, v := range x {
+			x[i] = convertYamlToJson(v)
+		}
+	}
+	return i
 }
