b6a722a925adc4667b6539dc6bcaf8702955063e
Kwandes <bz.jasiek@gmail.com>
2022-08-30 09:23:58 +0200

    Add encoding-decoding exercise

---
+++ lectures/lecture-duo/excode/index.js
@@ -0,0 +1,6 @@
+const data = "yeetsadtas898eo23iyudf1vhwqw08978eiutg23hiopuiy";
+console.log(data);
+const encoded = btoa(data);
+console.log(encoded);
+const decoded = atob(encoded);
+console.log(decoded);
