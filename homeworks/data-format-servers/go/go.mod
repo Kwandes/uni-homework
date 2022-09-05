d20f12d7753036073d885d845684a0f8a19e806a
Kwandes <bz.jasiek@gmail.com>
2022-09-05 10:52:24 +0200

    Add Go data server endpoints for JSON and Yaml

--- homeworks/data-format-servers/go/go.mod
+++ homeworks/data-format-servers/go/go.mod
@@ -2,7 +2,10 @@ module data-servers
 
 go 1.19
 
-require github.com/gin-gonic/gin v1.8.1
+require (
+	github.com/gin-gonic/gin v1.8.1
+	gopkg.in/yaml.v3 v3.0.1
+)
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
--- homeworks/data-format-servers/go/main.go
+++ homeworks/data-format-servers/go/main.go
@@ -7,6 +7,12 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"net/http"
+
+	"encoding/json"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v3"
 )
 
 var serverPort int = 3000
@@ -17,6 +23,8 @@ func main() {
 
 	router := gin.Default()
 	router.GET("/healthcheck", getServerStatus)
+	router.GET("/files/json", getJson)
+	router.GET("/files/yaml", getYaml)
 
 	router.Run("localhost:" + strconv.Itoa(serverPort))
 }
@@ -28,4 +36,61 @@ func getServerStatus(c *gin.Context) {
 	data["serverPort"] = serverPort
 	data["targetServerPort"] = otherServerPort
 	c.IndentedJSON(http.StatusOK, data)
+}
+
+type planetsData []map[string]interface{}
+
+func getJson(c *gin.Context) {
+	fmt.Println("===== JSON =====")
+
+	var data planetsData
+	// Json Data
+	jsonFile, err := ioutil.ReadFile("./files/planets.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(jsonFile, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(data)
+	c.IndentedJSON(0, data)
+}
+
+func getYaml(c *gin.Context) {
+	// YAML Data
+	fmt.Println("===== YAML =====")
+	dataYaml, err := ioutil.ReadFile("./files/planets.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var body interface{}
+	if err := yaml.Unmarshal([]byte(dataYaml), &body); err != nil {
+		log.Fatal(err)
+	}
+
+	body = convertYamlToJson(body)
+
+	if b, err := json.Marshal(body); err != nil {
+		log.Fatal(err)
+	} else {
+		c.IndentedJSON(0, body)
+		fmt.Printf("Output: %s\n", b)
+	}
+}
+
+func convertYamlToJson(i interface{}) interface{} {
+	switch x := i.(type) {
+	case map[interface{}]interface{}:
+		m2 := map[string]interface{}{}
+		for k, v := range x {
+			m2[k.(string)] = convertYamlToJson(v)
+		}
+		return m2
+	case []interface{}:
+		for i, v := range x {
+			x[i] = convertYamlToJson(v)
+		}
+	}
+	return i
 }
